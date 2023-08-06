91998b1f1d6274158ee871f5425722a47ace52c4
Karel <nagelkarel@gmail.com>
2023-08-06 10:49:21 +0300

    rm unused deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, sharpImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from '@astrojs/vercel/edge';
@@ -9,13 +9,5 @@ export default defineConfig({
     react(),
   ],
   output: "hybrid",
-  adapter: vercel({
-    imageService: true
-  }),
-  experimental: {
-    assets: true
-  },
-  image: {
-    service: sharpImageService(),
-  },
+  adapter: vercel({}),
 });
